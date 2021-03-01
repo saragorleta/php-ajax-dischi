30dc684cd287c9bcd8da846cb6a3a9097e491b2f
Sara <saragorleta@yahoo.com>
2021-03-01 17:39:06 +0100

    reinserimento array nel file db.php

---
+++ src/db.php
@@ -0,0 +1,38 @@
+<?php
+
+  $dischi =[
+    [
+      'image'=>"",
+      'autor'=>"michael jackson",
+      'title'=>"Thriller",
+      'year' =>"1920"
+
+    ],
+    [
+      'image'=>"",
+      'autor'=>"michael jackson",
+      'title'=>"ciao",
+      'year' =>"1920"
+
+    ],
+    [
+      'image'=>"",
+      'autor'=>"michael jackson",
+      'title'=>"ciao",
+      'year' =>"1920"
+
+    ],
+    [
+      'image'=>"",
+      'autor'=>"michael jackson",
+      'title'=>"ciao",
+      'year' =>"1920"
+
+    ]
+
+  ]
+
+
+
+
+ ?>
